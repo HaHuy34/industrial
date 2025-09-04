4e116616e59b9add37fc9a2682fa61ea9a50977c
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2025-09-04 07:20:05 +0700

    update code

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// Menu Products
+document.querySelector(".main-list-products").addEventListener("click", function (e) {
+  e.stopPropagation(); // Ngăn chặn click lan ra ngoài
+  document.querySelector(".list-pr").classList.toggle("active");
+});
+
+// Ẩn menu khi click ra ngoài
+document.addEventListener("click", function (e) {
+  const listProduct = document.querySelector(".list-product");
+  const listPr = document.querySelector(".list-pr");
+
+  // Nếu click ngoài vùng .list-product thì ẩn
+  if (!listProduct.contains(e.target)) {
+    listPr.classList.remove("active");
+  }
+});
+
 // Submenu
 document.querySelectorAll(".main-menu").forEach((item) => {
   item.addEventListener("click", function (e) {
@@ -28,3 +45,61 @@ buttons.forEach((btn) => {
     btn.classList.add("active");
   });
 });
+
+// Search
+const inputSearch = document.getElementById("ip-search");
+const products = document.querySelectorAll(".col-6, .col-sm-4, .col-md-3, .col-lg-3");
+
+inputSearch.addEventListener("keyup", function (event) {
+  let keyword = this.value.toLowerCase().trim();
+
+  // Nếu input trống → show hết
+  if (keyword === "") {
+    products.forEach(product => product.classList.remove("hidden"));
+    return;
+  }
+
+  // Nếu nhấn Enter → lọc
+  if (event.key === "Enter") {
+    products.forEach(product => {
+      let name = product.querySelector(".detail-product").textContent.toLowerCase();
+      if (name.includes(keyword)) {
+        product.classList.remove("hidden");
+      } else {
+        product.classList.add("hidden");
+      }
+    });
+  }
+});
+
+// Animation header
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  if (window.scrollY > 100) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+// Back to top
+const backToTop = document.getElementById("backToTop");
+
+// Hiện / ẩn nút khi scroll
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+// Click nút → cuộn mượt về đầu trang
+backToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
